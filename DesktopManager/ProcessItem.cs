63964a47d25aad67c45449c00981c394e8c9382e
Seumas Gordon <SJGordon1@student.fullsail.edu>
2020-08-27 18:58:55 -0400

    Added Classes for per process items, And classes for displaying information. Added XAML for UI. UI is scale able. #TODO Make Row 0 and Each Column have a box to help define.

---
+++ DesktopManager/ProcessItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopManager
+{
+    class ProcessItem
+    {
+        public Process Process { get; }
+        public int Id => Process.Id;
+        public string ProcessName => Process.ProcessName;
+
+        public ProcessItem(Process process)
+        {
+            Process = process;
+        }
+    }
+}
---
+++ DesktopManager/Viewer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using DesktopManager.Annotations;
+
+namespace DesktopManager
+{
+    class Viewer : INotifyPropertyChanged
+    {
+        public Viewer()
+        {
+            var ticker = new DispatcherTimer { Interval = TimeSpan.FromSeconds(5) };
+            ticker.Tick += UpdateProcess;
+            ticker.Start();
+        }
+
+        private Process selProcess;
+        public Process SelectProcess
+        {
+            get => selProcess;
+            set
+            {
+                selProcess = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ObservableCollection<ProcessItem> ProcessItems { get; } = new ObservableCollection<ProcessItem>();
+
+        public void UpdateProcess(object sender, EventArgs e)
+        {
+            var currentIds = ProcessItems.Select(processes => processes.Id).ToList();
+
+            foreach (var p in Process.GetProcesses())
+            {
+                if (!currentIds.Remove(p.Id))
+                {
+                    ProcessItems.Add(new ProcessItem(p));
+                }
+            }
+        }
+
+        public void KillProcess()
+        {
+            SelectProcess.Kill();
+        }
+
+        #region EventHandler
+        public event PropertyChangedEventHandler PropertyChanged;
+        [NotifyPropertyChangedInvocator]
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
