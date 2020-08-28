02fb0394d285d5ea28f2c59f858bf8ac25faf954
Seumas Gordon <SJGordon1@student.fullsail.edu>
2020-08-27 21:47:03 -0400

    Now shows time process has ran. if it has permission. #TODO Make time round to the hundreds place.

--- DesktopManager/ProcessItem.cs
+++ DesktopManager/ProcessItem.cs
@@ -12,10 +12,24 @@ namespace DesktopManager
         public Process Process { get; }
         public int Id => Process.Id;
         public string ProcessName => Process.ProcessName;
+        public string Time => GetRunningTime(Process);
 
         public ProcessItem(Process process)
         {
             Process = process;
+        }
+
+        public string GetRunningTime(Process p)
+        {
+            string returns = "00:00:00";
+            try
+            {
+                returns = (DateTime.Now - p.StartTime).ToString();
+            }
+            catch (Exception)
+            {
+            }
+            return returns;
         }
     }
 }
