b2f05f2738700a307082944284e1f424563a5d9b
Seumas Gordon <SJGordon1@student.fullsail.edu>
2020-09-10 13:52:28 -0400

    Added a setting file that will store the bindings for the isSelected for the combobox. Along with the bindings for the editable values. When clicking apply is checks the combobox for its selected index. #TODO make the IsSelected go off of bindings and finish apply.

--- DesktopManager/Settings.xaml.cs
+++ DesktopManager/Settings.xaml.cs
@@ -28,12 +28,14 @@ namespace DesktopManager
         {
             PlaceHolderWindow PHW = new PlaceHolderWindow();//Place Holder Window
             PHW.Show();
+            this.Close();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)//View
         {
             PlaceHolderWindow PHW = new PlaceHolderWindow();//Place Holder Window
             PHW.Show();
+            this.Close();
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)//Exit
@@ -43,6 +45,34 @@ namespace DesktopManager
 
         private void Button_Click_3(object sender, RoutedEventArgs e)//Save
         {
+            switch (MemoryViewSizeCombobox.SelectedIndex)
+            {
+                case 0://Auto
+                    break;
+                case 1://MB
+                    break;
+                case 2://KB
+                    break;
+                case 3://Byte
+                    break;
+                default://NA
+                    break;
+            }
+            switch (RefreshViewTimeCombobox.SelectedIndex)
+            {
+                case 0://.5
+                    break;
+                case 1://1
+                    break;
+                case 2://2
+                    break;
+                case 3://5
+                    break;
+                case 4://10
+                    break;
+                default://NA
+                    break;
+            }
             //Do checks if there was a change
             //if changed change values, and change IsSelected on combobox.
             this.Close();
---
+++ DesktopManager/SettingsOptions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesktopManager
+{
+    public class SettingsOptions
+    {
+        public bool boolMemoryViewAuto;
+        public bool boolMemoryViewMega;
+        public bool boolMemoryViewKilo;
+        public bool boolMemoryViewByte;
+
+        public bool boolRefreshViewHalf;
+        public bool boolRefreshViewOne;
+        public bool boolRefreshViewTwo;
+        public bool boolRefreshViewFive;
+        public bool boolRefreshViewTen;
+    }
+}
